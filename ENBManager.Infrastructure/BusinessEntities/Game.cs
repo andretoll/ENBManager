2ecbbd96309219ededf86a56e65e7b57e4f60c7d
Andre Toll <andretoll@outlook.com>
2020-04-28 08:41:59 +0200

    Added business entities

---
+++ ENBManager.Infrastructure/BusinessEntities/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ENBManager.Infrastructure.BusinessEntities
+{
+    public class Game
+    {
+        public Guid Uid { get; set; }
+        public string Title { get; set; }
+        public bool Active { get; set; }
+
+        public ICollection<Preset> Presets { get; set; }
+    }
+}
---
+++ ENBManager.Infrastructure/BusinessEntities/Preset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ENBManager.Infrastructure.BusinessEntities
+{
+    public class Preset
+    {
+        public Guid Uid { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<Image> Screenshots { get; set; }
+    }
+}
